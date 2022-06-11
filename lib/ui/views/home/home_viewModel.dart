06df94969a9e15ecd089ef6ebc7e36003060e16b
Hammad Khan <hammad.khan0312@gmail.com>
2022-06-11 14:11:27 +0500

    working

--- lib/ui/views/home/home_viewModel.dart
+++ lib/ui/views/home/home_viewModel.dart
@@ -2,5 +2,13 @@ import 'package:stacked/stacked.dart';
 
 class HomeViewModel extends BaseViewModel {
   String _title = "Home View";
-  String get title => _title;
+  String get title => '$_title $_counters';
+
+  int _counters = 0;
+  int get counters => _counters;
+
+  void increment() {
+    _counters++;
+    notifyListeners();
+  }
 }
